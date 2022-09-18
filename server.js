cbdfddf527459732be34ecf23e2d74ab7673e86e
Omar Rodriguez <omar.rguez26@gmail.com>
2022-09-18 14:08:28 -0400

    iniial express setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nickless-socialize-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
